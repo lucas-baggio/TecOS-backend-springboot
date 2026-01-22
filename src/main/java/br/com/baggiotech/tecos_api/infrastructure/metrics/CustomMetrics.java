18820429d0faf4efb925b65b4f4d810b30156a3a
Lucas Baggio <lucas.baggio.silva@gmail.com>
2026-01-22 12:32:02 -0300

    feat: adiciona métricas customizadas para Budget
    - Adiciona contadores para budgets criados, atualizados e deletados
    - Integra métricas com use cases de Budget

--- src/main/java/br/com/baggiotech/tecos_api/infrastructure/metrics/CustomMetrics.java
+++ src/main/java/br/com/baggiotech/tecos_api/infrastructure/metrics/CustomMetrics.java
@@ -11,6 +11,18 @@ public class CustomMetrics {
     private final Counter companiesCreatedCounter;
     private final Counter companiesUpdatedCounter;
     private final Counter companiesDeletedCounter;
+    private final Counter clientsCreatedCounter;
+    private final Counter clientsUpdatedCounter;
+    private final Counter clientsDeletedCounter;
+    private final Counter equipmentsCreatedCounter;
+    private final Counter equipmentsUpdatedCounter;
+    private final Counter equipmentsDeletedCounter;
+    private final Counter workOrdersCreatedCounter;
+    private final Counter workOrdersUpdatedCounter;
+    private final Counter workOrdersDeletedCounter;
+    private final Counter budgetsCreatedCounter;
+    private final Counter budgetsUpdatedCounter;
+    private final Counter budgetsDeletedCounter;
     private final MeterRegistry meterRegistry;
 
     public CustomMetrics(MeterRegistry meterRegistry) {
@@ -29,6 +41,66 @@ public class CustomMetrics {
                 .description("Total number of companies deleted")
                 .tag("application", "tecos-api")
                 .register(meterRegistry);
+
+        this.clientsCreatedCounter = Counter.builder("tecos_clients_created_total")
+                .description("Total number of clients created")
+                .tag("application", "tecos-api")
+                .register(meterRegistry);
+
+        this.clientsUpdatedCounter = Counter.builder("tecos_clients_updated_total")
+                .description("Total number of clients updated")
+                .tag("application", "tecos-api")
+                .register(meterRegistry);
+
+        this.clientsDeletedCounter = Counter.builder("tecos_clients_deleted_total")
+                .description("Total number of clients deleted")
+                .tag("application", "tecos-api")
+                .register(meterRegistry);
+
+        this.equipmentsCreatedCounter = Counter.builder("tecos_equipments_created_total")
+                .description("Total number of equipments created")
+                .tag("application", "tecos-api")
+                .register(meterRegistry);
+
+        this.equipmentsUpdatedCounter = Counter.builder("tecos_equipments_updated_total")
+                .description("Total number of equipments updated")
+                .tag("application", "tecos-api")
+                .register(meterRegistry);
+
+        this.equipmentsDeletedCounter = Counter.builder("tecos_equipments_deleted_total")
+                .description("Total number of equipments deleted")
+                .tag("application", "tecos-api")
+                .register(meterRegistry);
+
+        this.workOrdersCreatedCounter = Counter.builder("tecos_work_orders_created_total")
+                .description("Total number of work orders created")
+                .tag("application", "tecos-api")
+                .register(meterRegistry);
+
+        this.workOrdersUpdatedCounter = Counter.builder("tecos_work_orders_updated_total")
+                .description("Total number of work orders updated")
+                .tag("application", "tecos-api")
+                .register(meterRegistry);
+
+        this.workOrdersDeletedCounter = Counter.builder("tecos_work_orders_deleted_total")
+                .description("Total number of work orders deleted")
+                .tag("application", "tecos-api")
+                .register(meterRegistry);
+
+        this.budgetsCreatedCounter = Counter.builder("tecos_budgets_created_total")
+                .description("Total number of budgets created")
+                .tag("application", "tecos-api")
+                .register(meterRegistry);
+
+        this.budgetsUpdatedCounter = Counter.builder("tecos_budgets_updated_total")
+                .description("Total number of budgets updated")
+                .tag("application", "tecos-api")
+                .register(meterRegistry);
+
+        this.budgetsDeletedCounter = Counter.builder("tecos_budgets_deleted_total")
+                .description("Total number of budgets deleted")
+                .tag("application", "tecos-api")
+                .register(meterRegistry);
     }
 
     public void incrementCompaniesCreated() {
@@ -41,6 +113,54 @@ public class CustomMetrics {
 
     public void incrementCompaniesDeleted() {
         companiesDeletedCounter.increment();
+    }
+
+    public void incrementClientsCreated() {
+        clientsCreatedCounter.increment();
+    }
+
+    public void incrementClientsUpdated() {
+        clientsUpdatedCounter.increment();
+    }
+
+    public void incrementClientsDeleted() {
+        clientsDeletedCounter.increment();
+    }
+
+    public void incrementEquipmentsCreated() {
+        equipmentsCreatedCounter.increment();
+    }
+
+    public void incrementEquipmentsUpdated() {
+        equipmentsUpdatedCounter.increment();
+    }
+
+    public void incrementEquipmentsDeleted() {
+        equipmentsDeletedCounter.increment();
+    }
+
+    public void incrementWorkOrdersCreated() {
+        workOrdersCreatedCounter.increment();
+    }
+
+    public void incrementWorkOrdersUpdated() {
+        workOrdersUpdatedCounter.increment();
+    }
+
+    public void incrementWorkOrdersDeleted() {
+        workOrdersDeletedCounter.increment();
+    }
+
+    public void incrementBudgetsCreated() {
+        budgetsCreatedCounter.increment();
+    }
+
+    public void incrementBudgetsUpdated() {
+        budgetsUpdatedCounter.increment();
+    }
+
+    public void incrementBudgetsDeleted() {
+        budgetsDeletedCounter.increment();
     }
 
     public Timer.Sample startTimer() {
